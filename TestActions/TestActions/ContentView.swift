9871d6032b73067245810fc52048ece81018730a
CaioVicenzi <80120724+CaioVicenzi@users.noreply.github.com>
2024-06-22 10:32:23 -0300

    adicionando-texto

--- TestActions/TestActions/ContentView.swift
+++ TestActions/TestActions/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            Text("Lista de items")
             List {
                 ForEach(list, id: \.id) {element in
                     Text(element.title)
